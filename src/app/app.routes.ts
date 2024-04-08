c5b80de47ac50f8cf5cb2c85fa681ea25c5777e8
NguyenHuuHaiPhuoc <nguyenhuuhaiphuoc@gmail.com>
2024-04-08 11:10:20 +0700

    register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'nav', component: NavbarComponent}
+    {path: 'nav', component: NavbarComponent},
+    {path: 'register', component: RegisterComponent},
 ];
