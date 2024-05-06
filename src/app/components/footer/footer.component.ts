e6ba1a27b417e84c6475441c515d8fec1a1eba1c
NguyenHuuHaiPhuoc <nguyenhuuhaiphuoc@gmail.com>
2024-05-06 21:49:42 +0700

    coppy source form phuoc_dev

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,17 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-declare var $:any;
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
 })
-export class FooterComponent{
+export class FooterComponent {
 
-    backToTop () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    }
 }
